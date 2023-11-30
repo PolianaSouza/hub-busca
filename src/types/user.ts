cf0173f2c07d81070735173e4aafb138c7915305
Poliana Souza <polysantos88@gmail.com>
2023-11-30 12:50:27 -0300

    Criação de tipagem para o usuário

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export type UserProps = {
+  avatar_url: string;
+  name: string;
+  login: string;
+  location: string;
+  folowers: number;
+  following: number;
+  id: number;
+  public_repos: number;
+  repos_url: string;
+};
