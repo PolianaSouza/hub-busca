cbaa088f5e08daf21cee6b13191fe431962a2797
Poliana Souza <polysantos88@gmail.com>
2023-12-02 12:18:13 -0300

    style: deixando o navbar responsivo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Navilinks from "../navlinks/Navilinks";
 import styles from "./Header.module.css";
+import { Menu } from "lucide-react";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(true);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 800) {
+        setMenuOpen(true);
+      } else {
+        setMenuOpen(false);
+      }
+    });
+  }, []);
+
   const naviLinks = [
     {
       name: "Home",
@@ -19,21 +33,30 @@ export default function Header() {
   ];
 
   return (
-    <nav className={styles.navBar}>
-      <div>
+    <header className={styles.header}>
+      <div className={styles.logoAndMenu}>
         <Link href={"/"} className={styles.decoration}>
           <h1 className={styles.logo}>
             <strong className={styles.strong}>HUB</strong>usca
           </h1>
         </Link>
+        <Menu
+          className={styles.menuIcon}
+          size={40}
+          color="#ffffff"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
       </div>
-      <nav className={styles.containerLinks}>
-        {naviLinks.map((link) => (
-          <Navilinks key={link.name} href={link.path}>
-            {link.name}
-          </Navilinks>
-        ))}
-      </nav>
-    </nav>
+
+      {menuOpen && (
+        <nav className={styles.navbar}>
+          {naviLinks.map((link) => (
+            <Navilinks key={link.name} href={link.path}>
+              {link.name}
+            </Navilinks>
+          ))}
+        </nav>
+      )}
+    </header>
   );
 }
